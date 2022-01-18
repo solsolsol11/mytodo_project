8299802464cf45fbb8b88e0aa3924b95b432cd9a
solsolsol11 <ushio941@gmail.com>
2022-01-18 22:58:32 +0900

    모델링 진행중 마이그레이션 후 form으로 데이터를 보냈을 떄 안보내짐 수정중

--- su_list/forms.py
+++ su_list/forms.py
@@ -4,6 +4,13 @@ from su_list.models import Sulist
 
 
 class SulistForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['serious_no_urgent'].required = True
+        self.fields['serious_yes_urgent'].required = True
+        self.fields['no_serious_no_urgent'].required = True
+        self.fields['no_serious_yes_urgent'].required = True
+
     class Meta:
         model = Sulist
         fields = ['serious_no_urgent', 'serious_yes_urgent', 'no_serious_no_urgent', 'no_serious_yes_urgent']
--- su_list/views.py
+++ su_list/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from su_list.forms import SulistForm
+from su_list.models import Sulist
 
+@login_required
+def list(request:HttpRequest):
+    su_list = Sulist.objects.filter(user=request.user)
 
+    return render(request, "su_list/sulist_detail.html",{
+        "su_list": su_list
+    })
 
-
-
-def list(request):
-
-    return render(request, "su_list/sulist_detail.html")
