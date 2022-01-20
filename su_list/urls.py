109d130fc44351aa63d62dbf63fc1abf9e450b15
solsolsol11 <ushio941@gmail.com>
2022-01-20 11:27:44 +0900

    li 추가작업 완료및 각 li영역의 삭제구현 완료

--- su_list/urls.py
+++ su_list/urls.py
@@ -6,7 +6,15 @@ app_name = 'su_list'
 
 urlpatterns = [
 
-path('', views.green_list_, name="list"),
-path('create1/', views.green_list_create, name="green_list_create")
+    path('', views.all_list, name="list"),
+    path('test', views.test_case, name="test"),
+    path('create1/', views.green_list_create, name="green_list_create"),
+    path('create2/', views.red_list_create, name="red_list_create"),
+    path('create3/', views.gray_list_create, name="gray_list_create"),
+    path('create4/', views.gold_list_create, name="gold_list_create"),
+    path('/delete1_list/', views.green_list_delete, name='green_list_delete'),
+    path('/delete2_list/', views.red_list_delete, name='red_list_delete'),
+    path('/delete3_list/', views.gray_list_delete, name='gray_list_delete'),
+    path('/delete4_list/', views.gold_list_delete, name='gold_list_delete'),
 
 ]
--- su_list/views.py
+++ su_list/views.py
@@ -1,39 +1,101 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import QuerySet
-from django.http import HttpRequest
-from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from django.views.decorators.http import require_POST
 
 from su_list import models
-from su_list.forms import SeriousNoUrgentListForm, SeriousYesUrgentListForm
-
-def list(request: HttpRequest):
-    return render(request, "su_list/sulist_detail.html")
-
+from su_list.forms import SeriousNoUrgentListForm, SeriousYesUrgentListForm, NoSeriousNoUrgentForm, \
+    NoSeriousYesUrgentForm
+from su_list.models import SeriousNoUrgentList, SeriousYesUrgentList, NoSeriousNoUrgent, NoSeriousYesUrgent
 
 
-# 중요하지만 급하지 않아 list
+# 리스트 삭제
+# 중요하지만 급하지 않아 삭제
 @login_required
-def green_list_(request: HttpRequest):
+def green_list_delete(request: HttpRequest):
+    print('성공')
+
     green_list = models.SeriousNoUrgentList.objects.filter(user_id=request.user.id)
-    return render(request, "su_list/sulist_detail.html", {"green_list": green_list})
 
-# 중요하고 급해 list
+    green_list.delete()
+    print('성공')
+    messages.success(request, "삭제완료")
+
+    return redirect('su_list:list')
+
+# 리스트 삭제
+# 중요하고 급해 삭제
 @login_required
-def red_list_(request: HttpRequest):
-    red_list = models.SeriousNoUrgentList.objects.filter(user_id=request.user.id)
-    return render(request, "su_list/sulist_detail.html", {"red_list": red_list})
+def red_list_delete(request: HttpRequest):
+    print('성공')
 
+    red_list = models.SeriousYesUrgentList.objects.filter(user_id=request.user.id)
+
+    red_list.delete()
+    print('성공')
+    messages.success(request, "삭제완료")
+
+    return redirect('su_list:list')
+
+# 리스트 삭제
+# 중요하지도 않고 급하지도 않아 삭제
+@login_required
+def gray_list_delete(request: HttpRequest):
+    print('성공')
+
+    gray_list = models.NoSeriousNoUrgent.objects.filter(user_id=request.user.id)
+
+    gray_list.delete()
+    print('성공')
+    messages.success(request, "삭제완료")
+
+    return redirect('su_list:list')
+
+# 리스트 삭제
+# 중요하진 않은데 급해 삭제
+@login_required
+def gold_list_delete(request: HttpRequest):
+    print('성공')
+
+    gold_list = models.NoSeriousYesUrgent.objects.filter(user_id=request.user.id)
+
+    gold_list.delete()
+    print('성공')
+    messages.success(request, "삭제완료")
+
+    return redirect('su_list:list')
+
+
+# ALL LIST
+@login_required
+def all_list(request: HttpRequest):
+    green_list = models.SeriousNoUrgentList.objects.filter(user_id=request.user.id)
+    red_list = models.SeriousYesUrgentList.objects.filter(user_id=request.user.id)
+    gray_list = models.NoSeriousNoUrgent.objects.filter(user_id=request.user.id)
+    gold_list = models.NoSeriousYesUrgent.objects.filter(user_id=request.user.id)
+    return render(request, "su_list/sulist_detail.html", {
+        "green_list": green_list,
+        "red_list": red_list,
+        "gray_list": gray_list,
+        "gold_list": gold_list,
+
+    })
+
+
+# 테스트 케이스
+def test_case(request: HttpRequest):
+    return render(request, "su_list/test_case.html")
 
 
 # 중요하지만 급하지 않아 views
 @login_required
-def green_list_create(request:HttpRequest):
+def green_list_create(request: HttpRequest):
     form = SeriousNoUrgentListForm()
-
+    print(form)
     if request.method == "POST":
         form = SeriousNoUrgentListForm(request.POST)
         if form.is_valid():
@@ -41,11 +103,11 @@ def green_list_create(request:HttpRequest):
             article.user = request.user
             article.save()
             messages.success(request, "성공")
+            print("성공")
             return redirect('su_list:list')
 
     return render(request, 'su_list/sulist_detail.html', {
-        "form":form,
-
+        "form": form,
 
     })
 
@@ -54,14 +116,57 @@ def green_list_create(request:HttpRequest):
 @login_required
 def red_list_create(request: HttpRequest):
     form = SeriousYesUrgentListForm()
-
+    print(form)
     if request.method == "POST":
         form = SeriousYesUrgentListForm(request.POST)
         if form.is_valid():
-            article = form.save(commit=False)
-            article.user = request.user
-            article.save()
+            article2 = form.save(commit=False)
+            article2.user = request.user
+            article2.save()
             messages.success(request, "성공")
+            print("성공")
+            return redirect('su_list:list')
+
+    return render(request, 'su_list/sulist_detail.html', {
+        "form": form,
+
+    })
+
+
+# 중요하지도 않고 급하지도 않아 views
+@login_required
+def gray_list_create(request: HttpRequest):
+    form = NoSeriousNoUrgentForm()
+    print(form)
+    if request.method == "POST":
+        form = NoSeriousNoUrgentForm(request.POST)
+        if form.is_valid():
+            article3 = form.save(commit=False)
+            article3.user = request.user
+            article3.save()
+            messages.success(request, "성공")
+            print("성공")
+            return redirect('su_list:list')
+
+    return render(request, 'su_list/sulist_detail.html', {
+        "form": form,
+
+    })
+
+
+# 중요하진 않은데 급해 views
+@login_required
+def gold_list_create(request: HttpRequest):
+    form = NoSeriousYesUrgentForm()
+    print(form)
+    if request.method == "POST":
+        form = NoSeriousYesUrgentForm(request.POST)
+        if form.is_valid():
+            article4 = form.save(commit=False)
+            article4.user = request.user
+            article4.save()
+            messages.success(request, "성공")
+            print("성공")
             return redirect('su_list:list')
 
     return render(request, 'su_list/sulist_detail.html', {
